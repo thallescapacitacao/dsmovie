51e1d1bb1f0d1aef34a385c41ef2a4ca9d93d884
Thalles Carvalho <thallescapacitacao@gmail.com>
2022-03-13 07:29:43 -0300

    Routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,19 @@
+import Form from 'pages/Form';
+import Listing from 'pages/Listing';
 import Navbar from 'components/Navbar';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
-    <Navbar />
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Listing />} />
+        <Route path="/form">
+          <Route path=":movieId" element={<Form />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
