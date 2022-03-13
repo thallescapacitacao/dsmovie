b4b1e6976eea9e2f5116f00b990febb467c262c7
Thalles Carvalho <thallescapacitacao@gmail.com>
2022-03-13 10:56:48 -0300

    MovieCard

---
+++ frontend/src/components/MovieScore/index.tsx
@@ -0,0 +1,18 @@
+import MovieStars from 'components/MovieStars';
+import './styles.css';
+
+function MovieScore() {
+
+    const score = 1.5;
+    const count = 9;
+    
+    return (
+        <div className="dsmovie-score-container">
+            <p className="dsmovie-score-value">{score > 0 ? score.toFixed(1) : '-'}</p>
+            <MovieStars />
+            <p className="dsmovie-score-count">{count} avaliações</p>
+        </div>
+    );
+}
+
+export default MovieScore;
