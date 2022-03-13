3d28a2bdafb5163de05423fe67a0a2c69ced1434
Thalles Carvalho <thallescapacitacao@gmail.com>
2022-03-13 11:06:47 -0300

    Navigation buttons

--- frontend/src/pages/Form/index.tsx
+++ frontend/src/pages/Form/index.tsx
@@ -1,7 +1,8 @@
+import { Link } from 'react-router-dom';
 import './styles.css';
 
 function Form() {
-    
+
     const movie = {
         id: 1,
         image: 'https://www.themoviedb.org/t/p/w533_and_h300_bestv2/jBJWaqoSCiARWtfV0GlqHrcdidd.jpg',
@@ -9,7 +10,7 @@ function Form() {
         count: 2,
         score: 4.5
     };
-    
+
     return (
         <div className="dsmovie-form-container">
             <img className="dsmovie-movie-card-image" src={movie.image} alt={movie.title} />
@@ -34,7 +35,9 @@ function Form() {
                         <button type="submit" className="btn btn-primary dsmovie-btn">Salvar</button>
                     </div>
                 </form >
-                <button className="btn btn-primary dsmovie-btn mt-3">Cancelar</button>
+                <Link to="/">
+                    <button className="btn btn-primary dsmovie-btn mt-3">Cancelar</button>
+                </Link>
             </div >
         </div >
     );
